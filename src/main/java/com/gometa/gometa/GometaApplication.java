6ceea619a1b4e015491f94a2319528763ea70d02
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-05-11 21:46:22 -0300

    Inicio do projeto

---
+++ src/main/java/com/gometa/gometa/GometaApplication.java
@@ -0,0 +1,13 @@
+package com.gometa.gometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GometaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gometa/gometa/GometaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gometa.gometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
